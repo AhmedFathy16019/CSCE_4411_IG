51dc5833d7ddb6328df3e43f43c75e1b3d8b62a3
AhmedFathy16019 <ahmed_fathi@aucegypt.edu>
2023-10-27 20:01:56 +0200

    initial client for election

--- src/client.rs
+++ src/client.rs
@@ -1,47 +1,108 @@
-use std::net::UdpSocket;
+use std::net::{UdpSocket, SocketAddr, ToSocketAddrs};
 use std::thread;
 use std::time::{Duration, Instant};
 
-fn send_request_to_server(number: i64, operation_flag: u8) -> i64 {
-    let socket = UdpSocket::bind("0.0.0.0:0").expect("Failed to bind UDP socket");
+// To check if the response is election result or processed request
+enum ServerReply {
+    Address(SocketAddr),
+    Data(Vec<u8>),
+    None,
+}
 
-    let server_address = "127.0.0.1:8080";
-
-    // Send the request to the server (operation_flag + number)
+// For later bundling a request based on the feature - to be modified
+fn bundle_request(operation_flag: &u8, number: &i64) -> Vec<u8> {
     let mut request = Vec::new();
-    request.push(operation_flag);
+    request.push(*operation_flag);
     request.extend_from_slice(&number.to_be_bytes());
+    request
+}
+
+// send request to all servers to init an election
+fn send_request_to_servers(server_addresses: &Vec<SocketAddr>, data: &[u8]) {
+    // initialize socket
+    let socket = UdpSocket::bind("0.0.0.0:0")
+        .expect("Failed to bind UDP socket");
+    
+    // loop over all servers and send request to each of them
+    for &server_address in server_addresses {
+        socket
+            .send_to(data, server_address)
+            .expect("Failed to send data to server");
+    }    
+}
+
+// to send to one server after election ends
+fn send_request_to_server(server_addr: &SocketAddr, data: &[u8]) {
+    let socket = UdpSocket::bind("0.0.0.0:0")
+        .expect("Failed to bind UDP socket");
 
     socket
-        .send_to(&request, server_address)
+        .send_to(data, server_addr)
         .expect("Failed to send data to server");
+}
 
-    let mut buffer = [0; 8];
+// get responses to the socket
+fn read_response(socket: &UdpSocket) -> ServerReply {
+    let mut buffer = vec![0; 1024];
     // Receive the result from the server
-    socket
+    let (length, server_addr) = socket
         .recv_from(&mut buffer)
         .expect("Failed to receive data from server");
+    
+    // receive a flag to indicate the type of reply, 0 is election result (server address), 1 is the processed request's result
+    let reply_flag = buffer[0];
 
-    let result = i64::from_be_bytes(buffer);
-    result
+    // returning the appropriate result
+    match reply_flag {
+        0 => return ServerReply::Address(server_addr),
+        1 => {
+            return ServerReply::Data(buffer[1..length].to_vec());
+        },
+        _ => ServerReply::None
+    }
+}
+
+// handling election result or processed request
+fn handle_response(server_reply: &ServerReply, socket: &UdpSocket, data: &[u8]) {
+    match server_reply {
+        ServerReply::Address(address) => {
+            send_request_to_server(&address, data);
+            let server_reply = read_response(socket);
+            handle_response(&server_reply, socket, data);
+        },
+        ServerReply::Data(reply_data) => {
+            println!("Server replied with {:?}", i64::from_be_bytes(reply_data.as_slice().try_into().unwrap()));
+        },
+        ServerReply::None => {}
+    }
 }
 
 fn main() {
-    let number_of_requests = 1000;
+    let number_of_requests = 5;
     let delay_duration = Duration::from_secs(1);
-
     let mut total_duration = Duration::new(0, 0);
+
+    let client_socket = UdpSocket::bind("0.0.0.0:0").expect("failed to bind socket");
+
+    let mut server_addresses = Vec::new();
+    server_addresses.push("127.0.0.1:8080".to_socket_addrs().unwrap().next().unwrap());
+    server_addresses.push("127.0.0.1:8081".to_socket_addrs().unwrap().next().unwrap());
+    server_addresses.push("127.0.0.1:8082".to_socket_addrs().unwrap().next().unwrap());
 
     for i in 0..number_of_requests {
         let iteration_start = Instant::now();
 
-        let number_to_increment = 102;
-        let result_increment = send_request_to_server(number_to_increment, 0); // 0 for increment
-        println!("Server responded with increment: {}", result_increment);
+        let number_to_increment:i64 = 102;
+        let request = bundle_request(&0, &number_to_increment);
+        send_request_to_servers(&server_addresses, &request);
+        let server_reply = read_response(&client_socket);
+        handle_response(&server_reply, &client_socket, &request);
 
-        let number_to_decrement = 110;
-        let result_decrement = send_request_to_server(number_to_decrement, 1); // 1 for decrement
-        println!("Server responded with decrement: {}", result_decrement);
+        let number_to_decrement:i64 = 110;
+        let request = bundle_request(&1, &number_to_decrement);
+        send_request_to_servers(&server_addresses, &request);
+        let server_reply = read_response(&client_socket);
+        handle_response(&server_reply, &client_socket, &request);
 
         let iteration_time = iteration_start.elapsed();
         total_duration += iteration_time;
